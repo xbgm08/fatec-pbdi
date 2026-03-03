046d287c62ac9ceccdf3a9ec9cf4b34d508a9c21
xbgm08 <gabrielbraulino@gmail.com>
2026-03-03 11:11:08 -0300

    learn(ap08): encerra o codigo 2.6.1

--- ap08_estrutura_sequencial/script.sql
+++ ap08_estrutura_sequencial/script.sql
@@ -1,12 +1,34 @@
 DO $$
+-- a função random gera valores reais no intervalo 0 <= n < 1
 DECLARE
-    codigo INTEGER := 1;
-    nome_completo VARCHAR(100) := 'João da Silva';
-    salario NUMERIC(11, 2) := 20.5;
+    n1 NUMERIC(5,2);
+    n2 INTEGER;
+    limite_inferior INTEGER := 5;
+    limite_superior INTEGER := 17;
 BEGIN
-    RAISE NOTICE 'Meu código é %, me chamo % e meu salário é R$%.', codigo, nome_completo, salario;
+    n1 := random(); -- 0 <= n1 < 1
+    RAISE NOTICE '%', n1;
+    n1 := random() * 10; -- 0 <= n1 < 10
+    RAISE NOTICE '%', n1;
+    n1 := random() * 10 + 1; -- 1 <= n1 < 11
+    RAISE NOTICE '%', n1;
+
+    n2 := floor(random() * 10 + 1)::int; -- 1 <= n2 <= 10
+    RAISE NOTICE '%', n2;
+    n2 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::int; -- 5 <= n2 <= 17
+    RAISE NOTICE '%', n2;
 END;
 $$
+
+-- DO $$
+-- DECLARE
+--     codigo INTEGER := 1;
+--     nome_completo VARCHAR(100) := 'João da Silva';
+--     salario NUMERIC(11, 2) := 20.5;
+-- BEGIN
+--     RAISE NOTICE 'Meu código é %, me chamo % e meu salário é R$%.', codigo, nome_completo, salario;
+-- END;
+-- $$
 
 -- DO $$
 -- BEGIN
